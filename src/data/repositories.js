15a8b74f457efb8df2ad783a717c52afcd5e4095
Aigars Peda <aigarspeda@gmail.com>
2020-01-27 21:49:32 +0200

    Create repositories.js

---
+++ src/data/repositories.js
@@ -0,0 +1,122 @@
+export const repositories = [
+  {
+    name: "mezi",
+    code: "https://github.com/AigarsPeda/mezi",
+    website: "https://aigarspeda.github.io/mezi/",
+    language: "TypeScript",
+    library: "React",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/mezi/blob/master/screenshots/1.jpg",
+        2: "https://github.com/AigarsPeda/mezi/blob/master/screenshots/2.jpg",
+        3: "https://github.com/AigarsPeda/mezi/blob/master/screenshots/3.jpg",
+        4: "https://github.com/AigarsPeda/mezi/blob/master/screenshots/4.jpg"
+      }
+    ]
+  },
+  {
+    name: "color-app",
+    code: "https://github.com/AigarsPeda/color-app",
+    website: "https://color-select-app.herokuapp.com",
+    language: "JavaScript",
+    library: "React",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/color-app/blob/master/screenshots/Screenshot.png",
+        2: "https://github.com/AigarsPeda/color-app/blob/master/screenshots/Screenshot2.png",
+        3: "https://github.com/AigarsPeda/color-app/blob/master/screenshots/Screenshot4.png",
+        4: ""
+      }
+    ]
+  },
+  {
+    name: "medium-copy",
+    code: "https://github.com/AigarsPeda/medium-copy",
+    website: "",
+    language: "JavaScript",
+    library: "React",
+    images: [
+      {
+        1: "https://raw.githubusercontent.com/AigarsPeda/medium-copy/master/src/screenshots/screenshot2.png",
+        2: "https://raw.githubusercontent.com/AigarsPeda/medium-copy/master/src/screenshots/screenshot3.png",
+        3: "https://raw.githubusercontent.com/AigarsPeda/medium-copy/master/src/screenshots/screenshot4.png",
+        4: "https://raw.githubusercontent.com/AigarsPeda/medium-copy/master/src/screenshots/screenshot1.png"
+      }
+    ]
+  },
+  {
+    name: "e-commerce",
+    code: "https://github.com/AigarsPeda/e-commerce",
+    website: "https://shop-crwn-live.herokuapp.com/",
+    language: "JavaScript",
+    library: "React",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/e-commerce/blob/master/src/Screenshots/Screenshot.png",
+        2: "https://github.com/AigarsPeda/e-commerce/blob/master/src/Screenshots/Screenshot2.png",
+        3: "https://github.com/AigarsPeda/e-commerce/blob/master/src/Screenshots/Screenshot3.png",
+        4: "https://github.com/AigarsPeda/e-commerce/blob/master/src/Screenshots/Screenshot5.png"
+      }
+    ]
+  },
+  {
+    name: "face-detection-app",
+    code: "https://github.com/AigarsPeda/face-detection-app",
+    website: "https://aigars-face-detection.herokuapp.com/",
+    language: "JavaScript",
+    library: "React",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/face-detection-app/blob/master/screen-shots/4.png",
+        2: "https://github.com/AigarsPeda/face-detection-app/blob/master/screen-shots/1.png",
+        3: "https://github.com/AigarsPeda/face-detection-app/blob/master/screen-shots/2.png",
+        4: "https://github.com/AigarsPeda/face-detection-app/blob/master/screen-shots/3.png"
+      }
+    ]
+  },
+  {
+    name: "movies-website",
+    code: "https://github.com/AigarsPeda/movies-website",
+    website: "",
+    language: "JavaScript",
+    library: "",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/movies-website/blob/master/screenshots/Screenshot.png",
+        2: "https://github.com/AigarsPeda/movies-website/blob/master/screenshots/Screenshot2.png",
+        3: "https://github.com/AigarsPeda/movies-website/blob/master/screenshots/Screenshot3.png",
+        4: "https://github.com/AigarsPeda/movies-website/blob/master/screenshots/Screenshot4.png"
+      }
+    ]
+  },
+  {
+    name: "todo-react-typescript",
+    code: "https://github.com/AigarsPeda/todo-react-typescript",
+    website: "",
+    language: "TypeScript",
+    library: "React",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/todo-react-typescript/blob/master/screenshots/screenshot.png",
+        2: "",
+        3: "",
+        4: ""
+      }
+    ]
+  },
+  {
+    name: "dad-jokes-app",
+    code: "https://github.com/AigarsPeda/dad-jokes-app",
+    website: "https://aigarspeda.github.io/dad-jokes-app/",
+    language: "JavaScript",
+    library: "React",
+    images: [
+      {
+        1: "https://github.com/AigarsPeda/dad-jokes-app/blob/master/screenshots/Dad_Jokes.png",
+        2: "https://github.com/AigarsPeda/dad-jokes-app/blob/master/screenshots/Dad_Jokes2.png",
+        3: "",
+        4: ""
+      }
+    ]
+  }
+];
