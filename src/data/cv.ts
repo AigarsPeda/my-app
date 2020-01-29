a601a2a3a84ab464287bd671a51e3ae4525a2ca6
Aigars Peda <aigarspeda@gmail.com>
2020-01-29 09:36:35 +0200

    added cv

---
+++ src/data/cv.ts
@@ -0,0 +1,53 @@
+export const contacts = {
+  name: "Aigars Peda",
+  phone: "+73126478706",
+  email: "aigarspeda@gmail.com",
+  date: "22.06.1984.",
+  github: "https://github.com/AigarsPeda"
+};
+
+export const workExperience = {
+  work1: {
+    date: "06/206 - Present",
+    position: "Assistant Prosecutor",
+    address: "Prosecution Office of the Republica of Latvia, Riga"
+  },
+  work2: {
+    date: "09/2004 - 06/2007",
+    position: "Volleyball Player (Attacker Position)",
+    address: `Volleyball Club "Lase Robezsardze", Riga (Latvia)`
+  }
+};
+
+export const educationAndTraining = {
+  school1: {
+    date: "12/2018 - 02/2019",
+    qualification: "Programming Basics (JavaScript, Java, C#)",
+    institution: "Riga Coding School, Riga (Latvia)"
+  },
+  school2: {
+    date: "09/2003 - 06/2007",
+    qualification: "Unfinished Bachelors Degree",
+    institution: "Latvijas Policijas Akademija, Riga (Latvia)"
+  },
+  school3: {
+    date: "09/1991 - 06/2003",
+    qualification: "Secondary Education",
+    institution: "Rīgas Teikas vidusskola, Riga (Latvia)"
+  }
+};
+
+export const competences = {
+  languageSpoken: "Latvian",
+  otherLanguages: `English (Understanding: Independent User (B2)/Speaking/Writing: Basic User (B1))
+  Russian Understanding: Independent User (B2)/Speaking/Writing: Basic User (B2))
+  `,
+  socialSkillsAndCompetences:
+    "Ability to work effectively in a team setting, solution oriented mindset",
+  organizationalSkillsAndCompetences:
+    "Good organization skills, analytical thinking, ability to learn new programmes and processes quickly",
+  computerSkillsAndCompetences:
+    "Independent User of JavaScript, React, Adobe Lightroom, Final Cut Pro, Adobe Illustrator, Microsoft Office, Redux, Bootstrap, Material Design, Photoshop, Basic user of InDesign, CSS, SCSS, Typescript, MangoDB, FirebaseWorking Knowledge of WordPress",
+  additionalInformation:
+    "B category driving licence Semi-Professional Photographer, Photographer and Video Editor for www.piektdiena.com"
+};
