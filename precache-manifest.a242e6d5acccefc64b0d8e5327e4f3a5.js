14270d2f299c36620fdc35148a5c2e1fc361ba94
Aigars Peda <aigarspeda@gmail.com>
2020-01-31 19:11:26 +0200

    Updates

---
+++ precache-manifest.a242e6d5acccefc64b0d8e5327e4f3a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748e2f25c673e1537f2e513570075531",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7dbc26f901914e6adb7b",
+    "url": "/my-app/static/css/main.4fd15167.chunk.css"
+  },
+  {
+    "revision": "7c55b8faf0f2bb91614c",
+    "url": "/my-app/static/js/2.92c06bd0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my-app/static/js/2.92c06bd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7dbc26f901914e6adb7b",
+    "url": "/my-app/static/js/main.93d93d01.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "0c4dd193569e73b44be9e42b7f736509",
+    "url": "/my-app/static/media/main_img.0c4dd193.png"
+  }
+]);
