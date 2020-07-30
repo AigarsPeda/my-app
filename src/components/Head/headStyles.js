b497eb8690b10a2b423f1b99e4f3cbe97065a559
Aigars Peda <aigarspeda@gmail.com>
2020-07-30 11:05:39 +0300

    Update headStyles.js

--- src/components/Head/headStyles.js
+++ src/components/Head/headStyles.js
@@ -2,37 +2,37 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles({
   container: {
-    marginTop: "10px",
+    marginTop: "10px"
   },
   text: {
     fontSize: "20px",
     fontWeight: "bold",
-    margin: "0",
+    margin: "0"
   },
 
   //MONITORS
   "@media (min-width:1275px)": {
     container: {
       marginTop: "100px",
-      paddingTop: "45px",
-      height: "100vh",
+      paddingTop: "85px",
+      height: "100vh"
     },
     text: {
       marginTop: "5rem",
-      fontWeight: "bold",
-    },
+      fontWeight: "bold"
+    }
   },
 
   //IPADPRO
   "@media (min-width:1020px)": {
     container: {
-      marginTop: "0px",
+      marginTop: "0px"
     },
     text: {
       marginRight: "2rem",
       marginBottom: "2rem",
-      fontWeight: "bold",
-    },
+      fontWeight: "bold"
+    }
   },
   button: {
     textAlign: "center",
@@ -52,17 +52,17 @@ export const useStyles = makeStyles({
     alignItems: "center",
     "@media (min-width:1280px)": {
       padding: "20px 50px",
-      width: 290,
-    },
+      width: 290
+    }
   },
   imageContainer: {
     maxWidth: "100%",
-    display: "block",
+    display: "block"
   },
   image: {
     width: "100%",
     height: "auto",
-    transform: "scale(1.3)",
+    transform: "scale(1.3)"
   },
   arrow: {
     cursor: "pointer",
@@ -79,18 +79,18 @@ export const useStyles = makeStyles({
     fontSize: 40,
     animation: `$bounce 1s infinite`,
     "@media (min-width:1020px)": {
-      display: "block",
-    },
+      display: "block"
+    }
   },
   "@keyframes bounce": {
     "0%, 20%, 50%, 80%, 100%": {
-      transform: "translateY(0)",
+      transform: "translateY(0)"
     },
     "40%": {
-      transform: "translateY(-25px)",
+      transform: "translateY(-25px)"
     },
     "60%": {
-      transform: "translateY(-15px)",
-    },
-  },
+      transform: "translateY(-15px)"
+    }
+  }
 });
