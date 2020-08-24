70076c2ce0e9e82bb1239d80267a8a61f740865e
Aigars Peda <aigarspeda@gmail.com>
2020-08-24 18:08:15 +0300

    Update headStyles.ts

--- src/components/Head/headStyles.ts
+++ src/components/Head/headStyles.ts
@@ -93,7 +93,7 @@ export const useStyles = makeStyles((theme: Theme) =>
         width: "100%",
         position: "absolute",
         display: "block",
-        top: 485,
+        top: 495,
         left: 89,
         zIndex: -1,
         transform: "scale(4.4)"
