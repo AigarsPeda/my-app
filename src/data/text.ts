05d29c82d8d36d1790d0aaa9ca6805d0fc22a850
Aigars Peda <aigarspeda@gmail.com>
2021-03-15 16:50:57 +0200

    Update text.ts

--- src/data/text.ts
+++ src/data/text.ts
@@ -1,11 +1,11 @@
 export const mainText: MainText = {
   eng: {
     text:
-      "Hi! My name is Aigars, I am FULL STACK developer and I’m looking for a job",
+      "Hi! My name is Aigars, I am FRONT-END developer and I’m looking for a job",
     button: "Contact me"
   },
   lv: {
-    text: `Čau! Mani sauc Aigars, esmu FULL STACK izstrādātājs, un es meklēju darbu.`,
+    text: `Čau! Mani sauc Aigars, esmu FRONT-END izstrādātājs, un es meklēju darbu.`,
     button: "Sazinies ar mani"
   }
 };
